23e568ae75563d5ea603883bcec088a8ba6f775d
saikonuru <sairam.konuru@gmail.com>
2025-07-18 22:08:04 +0530

    Add vitest:ui and coverage support

--- notes.md
+++ notes.md
@@ -13,3 +13,13 @@ Unit
 Intergation
 E2E- End to End
 
+ctrl+shift+l = multi line edit
+ctrl+p - cmd palat ; search for file name
+
+
+ctrl+p + @ - search for a symbol in the file
+
+ctrl+p + : - go to line
+
+Alt+< - Go back
+Alt+> - Go Fwd
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { max } from "../src/intro";
+import { calculateAverage, factorial, fizzBuzz, max } from "../src/intro";
 describe("max", () => {
   it("should return first argument if it is greater", () => {
     expect(max(2, 1)).toBe(2);
@@ -10,5 +10,59 @@ describe("max", () => {
 
   it("should return first argument if both are equal", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("FizzBuzz", () => {
+  it("should return FizzBuzz if argument is divisible by 3 & 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if argument is only divisible by 3", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  it("should return Buzz if argument is only divisible by 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  it("should return the given argument if it is not divisible by 3 or 5", () => {
+    expect(fizzBuzz(11)).toBe("11");
+  });
+});
+
+describe("CalcAverage", () => {
+  it("should return Nan if array length is 0", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it("should calculate average of an array with 1 element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  it("should calculate average of an array with 2 elements", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  it("should calculate average of an array with 3 elements", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
+  });
+});
+
+describe("factorial", () => {
+  it("shoud return 0 if given 1", () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  it("shoud return 1 if given 1", () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  it("shoud return 6 if given 3", () => {
+    expect(factorial(3)).toBe(6);
+  });
+
+  it("shoud return 24 if given 4", () => {
+    expect(factorial(4)).toBe(24);
   });
 });
